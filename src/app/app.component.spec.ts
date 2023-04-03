e3fef16a132e9a4b0d34c1d5b924973726df11b3
Maitri Patel <maitri.07.2002@gmail.com>
2023-04-03 17:11:09 -0400

    Lab test done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '101308984-lab-test2-comp3133'`, () => {
+  it(`should have as title '101241116-lab-test2-comp3133'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('101308984-lab-test2-comp3133');
+    expect(app.title).toEqual('101241116-lab-test2-comp3133');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('101308984-lab-test2-comp3133 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('101241116-lab-test2-comp3133 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '101308984-lab-test2-comp3133';
+  title = '101241116-lab-test2-comp3133';
 }
